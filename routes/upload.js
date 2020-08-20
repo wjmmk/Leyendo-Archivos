e20a8715a02f44b8beec6dc0de7c5566c674bc8d
Wjmmk <wjmmk3@gmail.com>
2020-08-20 09:38:29 -0500

    Control en la carga del Achivo Plano

--- routes/upload.js
+++ routes/upload.js
@@ -1,13 +1,8 @@
 var express = require('express')
-var fs = require('fs')
-var path = require('path')
-var util = require('util')
 var multer = require('multer')
 
 var storage = multer.memoryStorage()
 var upload = multer({
-  /* dest: 'public/uploads',
-  limits: '5000000', */
   storage: storage
 })
 
@@ -33,28 +28,5 @@ router.post('/', upload.single('attachment'), function (req, res, next) {
    })();     
   
 })
-
-
-/* router.post('/', upload.single('attachment'), function (req, res, next) {
-  var resp = req.file;
-  console.log(resp.buffer.toString());  
-   return storeWithOriginalName(req.file)
-    .then(encodeURIComponent)
-    .then(encoded => {
-      res.redirect(`/upload/success?fileName=${encoded}`)
-    })
-    .catch(next)
-})
-
-// Aqui debo controlar MemoryStorage 
-function storeWithOriginalName (file) {
-  var fullNewPath = path.join(file.destination, file.originalname)
-  var rename = util.promisify(fs.rename)
-
-  return rename(file.path, fullNewPath)
-    .then(() => {
-      return file.originalname
-    })
-}  */
 
 module.exports = router
